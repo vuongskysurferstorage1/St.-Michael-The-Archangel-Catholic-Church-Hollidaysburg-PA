a3fc50001b0f05f2ebc49edd6c9499f87c7df23e
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2025-08-27 22:52:22 -0400

    360 Upload

---
+++ data.js
@@ -0,0 +1,629 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-st-michael-the-archangel-catholic-church-hollidaysburg-pa",
+      "name": "St. Michael The Archangel Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.06414376593334481,
+        "pitch": 0.08351828472885359,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0041571698181535055,
+          "pitch": -0.4126718651750636,
+          "rotation": 0,
+          "target": "1-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -0.009208631233573072,
+          "pitch": 0.7889880027988703,
+          "rotation": 0,
+          "target": "2-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.8150898880302222,
+          "pitch": 0.5941471640536271,
+          "rotation": 0,
+          "target": "3-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.3028940011864485,
+          "pitch": 0.606081554432123,
+          "rotation": 0,
+          "target": "4-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-st-michael-the-archangel-catholic-church-hollidaysburg-pa",
+      "name": "St. Michael The Archangel Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0648410342244663,
+          "pitch": -0.07529339223440346,
+          "rotation": 0,
+          "target": "0-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -0.017301375937790908,
+          "pitch": 1.3692740752903436,
+          "rotation": 0,
+          "target": "2-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.53500721193336,
+          "pitch": 0.9803193102822902,
+          "rotation": 0,
+          "target": "4-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 1.2640387759827014,
+          "pitch": 0.8614896048479004,
+          "rotation": 0,
+          "target": "3-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-st-michael-the-archangel-catholic-church-hollidaysburg-pa",
+      "name": "St. Michael The Archangel Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.992914615330063,
+          "pitch": -0.8256588474135977,
+          "rotation": 0,
+          "target": "0-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -3.0401984055271623,
+          "pitch": -1.4361241144232544,
+          "rotation": 0,
+          "target": "1-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 1.462972016978819,
+          "pitch": 0.09301208828783203,
+          "rotation": 0,
+          "target": "3-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -0.025586475665896558,
+          "pitch": 0.4002678853720454,
+          "rotation": 0,
+          "target": "5-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-st-michael-the-archangel-catholic-church-hollidaysburg-pa",
+      "name": "St. Michael The Archangel Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5096188628142873,
+          "pitch": -0.6268249937932246,
+          "rotation": 0,
+          "target": "0-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -0.941492578269262,
+          "pitch": -0.013005804799451681,
+          "rotation": 0,
+          "target": "2-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-st-michael-the-archangel-catholic-church-hollidaysburg-pa",
+      "name": "St. Michael The Archangel Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.149761728407996,
+          "pitch": -0.6853135880391417,
+          "rotation": 0,
+          "target": "0-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 2.4359195656600257,
+          "pitch": -1.2647116995171253,
+          "rotation": 0,
+          "target": "1-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-st-michael-the-archangel-catholic-church-hollidaysburg-pa",
+      "name": "St. Michael The Archangel Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0634810499339657,
+          "pitch": -0.4639444071412129,
+          "rotation": 0,
+          "target": "2-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.001137245816028809,
+          "pitch": 0.2612462395614621,
+          "rotation": 0,
+          "target": "6-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.004813020207857122,
+          "pitch": 0.03353245149006767,
+          "rotation": 0,
+          "target": "8-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-st-michael-the-archangel-catholic-church-hollidaysburg-pa",
+      "name": "St. Michael The Archangel Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.122825728461402,
+          "pitch": 0.5426045424249093,
+          "rotation": 0,
+          "target": "5-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -3.1284945683496925,
+          "pitch": -0.04818276389138987,
+          "rotation": 0,
+          "target": "2-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -0.0066665769124618635,
+          "pitch": 0.0824773894333255,
+          "rotation": 0,
+          "target": "8-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-st-michael-the-archangel-catholic-church-hollidaysburg-pa",
+      "name": "St. Michael The Archangel Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.010061365452102677,
+          "pitch": 0.4620415533522255,
+          "rotation": 0,
+          "target": "8-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -0.004739252337049749,
+          "pitch": -0.019300079800800063,
+          "rotation": 0,
+          "target": "10-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -3.0830573069428944,
+          "pitch": 0.7373724310681737,
+          "rotation": 0,
+          "target": "6-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-st-michael-the-archangel-catholic-church-hollidaysburg-pa",
+      "name": "St. Michael The Archangel Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1193746320822306,
+          "pitch": -0.5567055344697494,
+          "rotation": 0,
+          "target": "7-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -3.1193803116981584,
+          "pitch": 0.10334035382936335,
+          "rotation": 0,
+          "target": "6-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.009389406719693127,
+          "pitch": -0.36599939076484844,
+          "rotation": 0,
+          "target": "10-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.006383823237577246,
+          "pitch": 0.19544698207162092,
+          "rotation": 0,
+          "target": "9-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-st-michael-the-archangel-catholic-church-hollidaysburg-pa",
+      "name": "St. Michael The Archangel Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09853662201172497,
+          "pitch": -1.311627509204591,
+          "rotation": 0,
+          "target": "10-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.0010556483624704072,
+          "pitch": -0.013323808169527496,
+          "rotation": 0,
+          "target": "6-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.07536312341424534,
+          "pitch": 0.15015356511612765,
+          "rotation": 0,
+          "target": "8-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -0.0006661362223550071,
+          "pitch": -0.2914000893481532,
+          "rotation": 0,
+          "target": "7-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-st-michael-the-archangel-catholic-church-hollidaysburg-pa",
+      "name": "St. Michael The Archangel Catholic Church Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7224139296190195,
+          "pitch": 1.3590953327229052,
+          "rotation": 0,
+          "target": "9-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": -0.01571980087793534,
+          "pitch": 0.7046918549797478,
+          "rotation": 0,
+          "target": "8-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.01401120357770047,
+          "pitch": 0.15067056128329526,
+          "rotation": 0,
+          "target": "7-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        },
+        {
+          "yaw": 0.008383110290481,
+          "pitch": -0.25240692486112337,
+          "rotation": 0,
+          "target": "7-st-michael-the-archangel-catholic-church-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
